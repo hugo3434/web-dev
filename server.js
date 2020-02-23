faf42c79f36a5153bb726584630661dcf44a8165
hugo3434 <16218515@life.hkbu.edu.hk>
2020-02-23 22:19:47 +0800

    test

--- server.js
+++ server.js
@@ -1,29 +1,31 @@
-if(process.env.NODE_ENV !== 'prodution'){
+  
+if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config()
-}
-
-const express = require('express')
-const app = express()
-const expressLayouts = require('express-ejs-layouts')
-
-const indexRouter = require('./routes/index') //tell server the directory of the router
-
-app.set('view-engine', 'ejs') // set templating language https://expressjs.com/en/guide/using-template-engines.html
-app.set('views', __dirname + '/views') //set views directory
-app.set('layouts', 'layouts/layout') //set layouts directory
-app.use(expressLayouts) //use expressLayouts
-app.use(express.static('public')) //serve images, CSS files, and JavaScript files in a directory named public.
-
-const mongoose = require('mongoose') //import mongoose
-mongoose.connect(process.env.DATABASE_URL, {
-    useNewUrlParser: true
-}) //connect to mongoose
-const db = mongoose.connection
-db.on('error', err => {
-    console.error(err)
-}) //check error
-db.once('open', () => console.log('Connected to mongoose')) //check successful
-
-app.use('/', indexRouter) //first argu is directory, second argu is name of the router
-
-app.listen(process.env.PORT || 3000) //Binds and listens for connections on the specified host and port.
+  }
+  
+  const express = require('express')
+  const app = express()
+  const expressLayouts = require('express-ejs-layouts')
+  const bodyParser = require('body-parser')
+  
+  const indexRouter = require('./routes/index')
+  const authorRouter = require('./routes/authors')
+  
+  app.set('view engine', 'ejs')
+  app.set('views', __dirname + '/views')
+  app.set('layout', 'layouts/layout')
+  app.use(expressLayouts)
+  app.use(express.static('public'))
+  app.use(bodyParser.urlencoded({ limit: '10mb', extended: false }))
+  
+  const mongoose = require('mongoose')
+  mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+  const db = mongoose.connection
+  db.on('error', error => console.error(error))
+  db.once('open', () => console.log('Connected to Mongoose'))
+  
+  app.use('/', indexRouter)
+  app.use('/authors', authorRouter)
+  
+  app.listen(process.env.PORT || 3000)
+  
